d46c8265f64fc5be53b6e50b3d483d711d76da7a
nicolasGP <dam30@sprinde.com>
2026-03-13 11:46:03 +0100

    Guardado de videos y llamado

--- app/routers/media.py
+++ app/routers/media.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter,HTTPException,File, UploadFile
-from app.services.media import uploadImageCover,getCoverById,getCoverByIdPost
+from fastapi.responses import StreamingResponse 
+from app.services.media import *
 import os
 import shutil
 
@@ -15,6 +16,7 @@ ALLOWED_TYPES_VIDEO = {
     "video/mp4", "video/webm"
 }
 MAX_SIZE = 100 * 1024 * 1024
+MAX_SIZE_VIDEO = 100 * 1024 * 1024
 
 @app.post("/upload/image-cover")
 async def upload_image_cover(post_id:int,file: UploadFile = File(...)):
@@ -45,3 +47,46 @@ def get_cover_by_id_post(post_id: int):
 
     return media
     
+
+@app.post("/upload/video")
+async def upload_video(post_id: int, file: UploadFile = File(...)):
+    
+
+    if file.content_type not in ALLOWED_TYPES_VIDEO:
+        raise HTTPException(400, "Tipo de archivo no permitido")
+    if file.size > MAX_SIZE_VIDEO:
+        raise HTTPException(400, "Tamaño excedido de 500MB")
+
+    content = await file.read()
+
+    video = uploadVideo(post_id, file.filename, file.content_type, content, len(content))
+
+    return {"filename": file.filename, "result": video}
+
+
+@app.get("/media/video/{video_id}")
+def get_video(video_id: int):
+
+    video = getVideosById(video_id)
+    print(video)
+    if not video:
+        raise HTTPException(404, "Video no encontrado")
+
+    data = video["data"]
+    if isinstance(data, str):
+        data = data.encode("latin-1")
+
+    def video_stream():
+        chunk_size = 1024 * 1024  # 1MB por chunk
+        for i in range(0, len(data), chunk_size):
+            yield data[i:i + chunk_size]
+
+    return StreamingResponse(
+        video_stream(),
+        media_type=video["mime_type"],  # ✅ que sea "video/mp4"
+        headers={
+            "Content-Disposition": f"inline; filename={video['filename']}",
+            "Content-Length": str(len(data)),
+            "Accept-Ranges": "bytes"
+        }
+    )
--- app/services/media.py
+++ app/services/media.py
@@ -1,7 +1,7 @@
 from app.util import get_connection
 import base64
 
-
+#Covers
 def uploadImageCover(post_id:int,filename:str,mime_type:str,data:bytes,size:int):
 
     try:
@@ -50,4 +50,45 @@ def getCoverByIdPost( post_id: int) :
                     row["data_url"] = f"data:{row['mime_type']};base64,{b64}"
                     del row["data"]  # eliminar el blob crudo de la respuesta
                 return  row
+            
+#Video
+
+def uploadVideo(post_id:int,filename:str,mime_type:str,data:bytes,size:int):
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """ INSERT INTO videos (post_id, filename, mime_type, data, size)
+                    VALUES (%s, %s, %s, %s, %s)""",(post_id,filename,mime_type,data,size)
+                )
+                connection.commit
+                
+        return "Video Guardada"
+    except Exception as e:
+        raise e
+
+
+def getVideosByPost(post_id: int):
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT id, post_id, filename, mime_type, size, created_at FROM videos WHERE post_id = %s",
+                    (post_id,)
+                )
+                return cursor.fetchall()
+    except Exception as e:
+        raise e
+def getVideosById(id: int):
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT id, post_id, filename, mime_type,data, size, created_at FROM videos WHERE id = %s",
+                    (id,)
+                )
+                return cursor.fetchone()
+    except Exception as e:
+        raise e
 
